0e28534f49166a5da74d8188fe9c9a0fc01d276a
MuhadJasiR <alijasir051@gmail.com>
2025-04-10 18:10:50 +0530

    db update

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,11 +1,11 @@
 import {Pool} from 'pg';
 
 const pool = new Pool ({
-    user:'postgres',
-    password:'admin',
-    host:'localhost',
-    port:5432,
-    database:'chatDb'
+    user:  'postgres',
+    password:  'admin',
+    host:  'localhost',
+    port: 5432,
+    database: 'chatDb'
 })
 
 export default pool;
